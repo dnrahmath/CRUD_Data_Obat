fc53569385308b1e3ed19ae087c45a22accbbe27
Rahmat <dn.rahmath@gmail.com>
2021-04-15 13:20:29 +0700

    twst

--- CRUD_Data_Obat.java
+++ CRUD_Data_Obat.java
@@ -16,7 +16,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import java.awt.FlowLayout;
 
-//ini bertambah 1 dua tiga 4 fffgghh
+//ini bertambah 1 dua tiga 4 
 
 //button
 import java.awt.event.*; 
